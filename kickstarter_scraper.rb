ca5af6e25f4bd44ee4ac5cd1ed104bd9f2300fc2
Solomon Feldman <solomonrfeldman@gmail.com>
2019-05-15 21:15:02 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,7 +5,9 @@ require_relative 'fixtures/kickstarter'
 # projects: kickstarter.css("li.project.grid_4")
 
 def create_project_hash
-  
+  html = File.read('fixtures/kickstarter.html')
+ 
+  kickstarter = Nokogiri::HTML(html)
 
     
 end
